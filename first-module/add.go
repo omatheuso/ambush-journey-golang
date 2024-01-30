37becc6e9595b8dc8c3d4273d0a8259d0a669cff
Matheus de Oliveira <matheuscdeoliveira@gmail.com>
2024-01-30 16:28:09 -0300

    first go module

---
+++ first-module/add.go
@@ -0,0 +1,20 @@
+// Modify the Hello World program you made to contain a variable with your name, and then print “Hello, <name>” where “name” contains that value
+// Write a function that takes two integers as parameters and returns the sum of those two integers. Print this in the console as well.
+
+package main
+
+// import "fmt"
+
+func add(number1 int, number2 int) int {
+	return number1 + number2
+}
+
+// func main() {
+// 	var name string = "Matheus"
+// 	fmt.Printf("Hello, %v!\n", name)
+
+// 	var number1 int = 29
+// 	var number2 int = 40
+
+// 	fmt.Printf("Your sum result is %v :o\n", add(number1, number2))
+// }
---
+++ first-module/go.mod
@@ -0,0 +1,3 @@
+module add.go
+
+go 1.21.6
